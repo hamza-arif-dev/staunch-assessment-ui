2fb2b118f59f979d89c76089a03dce1ff6952ef5
Hamza Arif <hamzaarf.dev@gmail.com>
2024-08-18 01:59:55 +0500

    chore: config chakra provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,15 @@
+import { AppProvider, Box, Center } from "./components";
+import { Logo } from "./assets";
+
 export default function App() {
-  return <>App</>;
+  return (
+    <AppProvider>
+      <Center>
+        <Logo />
+      </Center>
+      <Box w="full" h="full" p={6}>
+        Bill Component  
+      </Box>
+    </AppProvider>
+  );
 }
