d8e6435b755470b2fc193d2afd28b5154dc24bda
Hamza Arif <hamzaarf.dev@gmail.com>
2024-08-18 01:47:35 +0500

    chore: add assets from figma

---
+++ src/assets/Plus.tsx
@@ -0,0 +1,19 @@
+export function Plus() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="20"
+      height="20"
+      fill="none"
+      viewBox="0 0 20 20"
+    >
+      <path
+        stroke="#fff"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="1.67"
+        d="M10 4.167v11.666M4.167 10h11.666"
+      ></path>
+    </svg>
+  );
+}
---
+++ src/assets/index.ts
@@ -0,0 +1,3 @@
+export { Delete } from "./Delete";
+export { Logo } from "./Logo";
+export { Plus } from "./Plus";
